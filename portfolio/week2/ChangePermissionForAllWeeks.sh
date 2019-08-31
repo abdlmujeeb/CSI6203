5120ce123e4158b7903fcbe0281579eb96a1bf59
abdlmujeeb <mujeeb.abdl2@gmail.com>
2019-08-31 14:19:04 -0700

    Completing the pending tasks till week 5

---
+++ portfolio/week2/ChangePermissionForAllWeeks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo 'Updating permissions of files'
+chmod +x ~/CSI6203/portfolio/"week1"/*.sh
+chmod +x ~/CSI6203/portfolio/"week2"/*.sh
+cd ~/CSI6203/
+tree
+echo 'Done'
---
+++ portfolio/week4/colorTest.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo -e "\033[31mEROOR\033[0m"
+exit 0
